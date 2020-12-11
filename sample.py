96a6c478a717e23739ac863267a2642c760fd332
Suraj Shrestha <syuraj@gmail.com>
2020-12-11 12:03:34 -0500

    add pltstyle

---
+++ sample.py
@@ -0,0 +1,12 @@
+# %% Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.metrics import r2_score, median_absolute_error, mean_absolute_error, mean_squared_log_error
+
+plt.style.use('./plot.mplstyle')
+
